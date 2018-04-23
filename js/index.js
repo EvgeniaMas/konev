7d9bde17f3320f3db47a026f21b91cbf2c266a04
Евгения Масликова < >
2018-04-23 09:29:27 +0300

    script fix

--- js/index.js
+++ js/index.js
@@ -9,27 +9,32 @@ $(".next").click(function(){
 	animating = true;
 	
 	current_fs = $(this).parent();
-	next_fs = $(this).parent().next();
 	
+	next_fs = $(this).parent().next();
+	// var number_step = $("#progressbar li").eq($("fieldset").index(next_fs));
+
 	//activate next step on progressbar using the index of next_fs
 	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
 	
 	//show the next fieldset
+	     
 	next_fs.show(); 
+
 	//hide the current fieldset with style
 	current_fs.animate({opacity: 0}, {
+
 		step: function(now, mx) {
 			//as the opacity of current_fs reduces to 0 - stored in "now"
 			//1. scale current_fs down to 80%
 			scale = 1 - (1 - now) * 0.2;
-			//2. bring next_fs from the right(50%)
-			left = (now * 50)+"%";
+			//2. bring next_fs from the right(30%)
+			left = (now * 30)+"%";
 			//3. increase opacity of next_fs to 1 as it moves in
 			opacity = 1 - now;
 			current_fs.css({'transform': 'scale('+scale+')'});
 			next_fs.css({'left': left, 'opacity': opacity});
 		}, 
-		duration: 800, 
+		duration: 1000, 
 		complete: function(){
 			current_fs.hide();
 			animating = false;
@@ -38,42 +43,54 @@ $(".next").click(function(){
 		easing: 'easeInOutBack'
 	});
 });
+    
 
-$(".previous").click(function(){
-	if(animating) return false;
-	animating = true;
+$("#send").click(function(){
+    var form = $('#msform');
+     form.find('.compulsory').addClass('empty_field');
+   
+        form.find('.compulsory').each(function(){
+        if($(this).val() != ''){
+        $(this).removeClass('empty_field');
+        } else {
+        $(this).addClass('empty_field');        
+        }
+       var sizeEmpty = form.find('.empty_field').size();
+ 
+
+	if(sizeEmpty > 0){
+        form.find('.empty_field').css({'border-color':'#d8512d'});
+         
+          return false;
+        }
+        else{
+        	
+        $('.form_container').hide('slow');
+	    $('#progressbar').hide('slow');	
+         $('#letter_sent').show('slow');
+	     
+        }
 	
-	current_fs = $(this).parent();
-	previous_fs = $(this).parent().prev();
-	
-	//de-activate current step on progressbar
-	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-	
-	//show the previous fieldset
-	previous_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale previous_fs from 80% to 100%
-			scale = 0.8 + (1 - now) * 0.2;
-			//2. take current_fs to the right(50%) - from 0%
-			left = ((1-now) * 50)+"%";
-			//3. increase opacity of previous_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({'left': left});
-			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
-		}, 
-		duration: 800, 
-		complete: function(){
-			current_fs.hide();
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
-	});
+});
 });
 
-$(".submit").click(function(){
-	return false;
-})
+
+        
+
+
+ 
+   $(document).ready(function(){
+    $('.get_offer').click( function(){ 
+    var scroll_el = $(this).attr('href'); 
+        if ($(scroll_el).length != 0) { 
+        $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 2000); // 
+        }
+        return false; 
+    });
+});
+
+$('#turnOn_video').on('click', function(){
+$('.video_block__content').hide('slow');
+$('#video').show('slow');
+$('#video')[0].play();
+});
