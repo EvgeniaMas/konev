0b8d11e89f01fbc72591d25a5a6adfdad1d4e1b0
Евгения Масликова < >
2018-04-26 19:56:18 +0300

    fix

--- js/index.js
+++ js/index.js
@@ -4,47 +4,67 @@ var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
 var animating; //flag to prevent quick multi-click glitches
 
+
+
+var count = 1;
+var current_fs = $('.question');
+// current_fs.fadeIn('1000');
+
 $(".next").click(function(){
-	if(animating) return false;
-	animating = true;
+	// if(animating) return false;
+	// animating = true;
+
+  
 	
-	current_fs = $(this).parent();
-	
-	next_fs = $(this).parent().next();
+	// var current_fs = $(this).parent();
+
+	next_fs = current_fs.next();
+  $(current_fs).removeClass('question');
+  next_fs.addClass('question');
 	// var number_step = $("#progressbar li").eq($("fieldset").index(next_fs));
 
 	//activate next step on progressbar using the index of next_fs
 	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
 	
 	//show the next fieldset
-	     
-	next_fs.show('slow'); 
+	current_fs.hide('slow');     
+	next_fs.fadeIn(1000); 
+   count++;
+    if (count>3){ 
+    $(".next").hide('slow');
+     $('#send').fadeIn(1000); 
+   }
+    
+    // alert(count);
+
 
 	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
+	// current_fs.animate({opacity: 0}, {
 
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale current_fs down to 80%
-			scale = 1 - (1 - now) * 0.2;
-			//2. bring next_fs from the right(30%)
-			left = (now * 90)+"%";
-			//3. increase opacity of next_fs to 1 as it moves in
-			opacity = 1 - now;
-			// current_fs.css({'transform': 'scale('+scale+')'});
-			next_fs.css({'opacity': opacity});
-		}, 
-		duration: 100, 
-		complete: function(){
-			current_fs.hide('slow');
+	// 	step: function(now, mx) {
+	// 		//as the opacity of current_fs reduces to 0 - stored in "now"
+	// 		//1. scale current_fs down to 80%
+	// 		scale = 1 - (1 - now) * 0.2;
+	// 		//2. bring next_fs from the right(30%)
+	// 		left = (now * 90)+"%";
+	// 		//3. increase opacity of next_fs to 1 as it moves in
+	// 		opacity = 1 - now;
+	// 		// current_fs.css({'transform': 'scale('+scale+')'});
+	// 		next_fs.css({'opacity': opacity});
+	// 	}, 
+	// 	duration: 100, 
+	// 	complete: function(){
+	// 		current_fs.hide('slow');
 
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutElastic'
-	});
+	// 		animating = false;
+	// 	}, 
+	// 	//this comes from the custom easing plugin
+	// 	easing: 'easeInOutElastic'
+	// });
+current_fs = next_fs;
+
 });
-    
+   
 
 $("#send").click(function(){
     var form = $('#msform');
@@ -64,13 +84,14 @@ $("#send").click(function(){
          
           return false;
         }
-        else{
+     //    else{
         	
-        $('#msform').hide('slow');
-	    $('#progressbar').hide('slow');	
-         $('#letter_sent').show('slow');
+     //    $('#msform').hide('slow');
+     //    console.log("Done");
+	    // $('#progressbar').hide('slow');	
+     //    $('#letter_sent').show('slow');
 	     
-        }
+     //    }
 	
 });
 });
